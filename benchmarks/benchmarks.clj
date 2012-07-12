35bc6d315601beb3ee67446f1e6e0b8ed170158b
Peter Taoussanis <ptaoussanis@gmail.com>
2012-07-12 15:34:54 +0700

    Simplify benchmarks.

--- benchmarks/benchmarks.clj
+++ benchmarks/benchmarks.clj
@@ -1,101 +1,88 @@
 (ns taoensso.carmine.benchmarks
   {:author "Peter Taoussanis"}
-  (:require [redis.core       :as redis-clojure]
-            [clj-redis.client :as clj-redis]
-            [accession.core   :as accession]
-            [taoensso.carmine :as carmine]))
+  (:require [redis.core             :as redis-clojure]
+            [clj-redis.client       :as clj-redis]
+            [accession.core         :as accession]
+            [taoensso.carmine       :as carmine]
+            [taoensso.carmine.utils :as utils]))
 
-(defn make-benching-options
-  [{:keys [requests clients data-size carmine-pool carmine-spec
-           clj-redis-pool accession-spec] :as opts}]
-  (let [merged-opts (merge {:requests  10000
-                            :clients   5
-                            :data-size 32
-                            :data-key  "carmine:temp:benchmark:data-key"} opts)]
-    (assoc merged-opts
-      :data (apply str (repeat (:data-size merged-opts) "x")))))
+;;;; Bench params
 
-(defmacro time-requests
-  "Executes threaded requests and returns total execution time in msecs or
-  \"DNF\" if there was a problem."
-  [opts & body]
-  `(do (dotimes [_# (int (/ (:requests ~opts) 4))] ~@body) ; Warmup
+(def data     (apply str (repeat 32 "x")))
+(def data-key "carmine:temp:benchmark:data-key")
 
-       (let [start-time#          (System/nanoTime)
-             requests-per-client# (int (/ (:requests ~opts)
-                                          (:clients  ~opts)))]
-         (try (->> (fn [] (future (dotimes [_# requests-per-client#] ~@body)))
-                   (repeatedly (:clients ~opts))
-                   doall
-                   (map deref)
-                   dorun)
-              (Math/round (/ (- (System/nanoTime) start-time#) 1000000.0))
-              (catch Exception e# (println "Exception: " e#) "DNF")
-              (finally (Thread/sleep 500))))))
+(defmacro bench [& body] `(utils/bench 10000 (do ~@body)
+                                       ;; :num-threads 5
+                                       :warmup-laps 10000))
 
-(defmacro wrc [& body]
-  `(time-requests ~'opts (redis-clojure/with-server {} ~@body)))
+;;;; Client rigs
+
+;;; redis-clojure
+
+(defmacro wrc [& body] `(bench (redis-clojure/with-server {} ~@body)))
 
 (defn bench-redis-clojure
-  [{:keys [data-key data] :as opts}]
+  []
   (println "Benching redis-clojure...")
   {:ping (wrc (redis-clojure/ping))
    :set  (wrc (redis-clojure/set data-key data))
    :get  (wrc (redis-clojure/get data-key))})
 
-(defmacro wcr [& body]
-  `(time-requests ~'opts (-> ~'db ~@body)))
+;;; clj-redis
+
+(defonce clj-redis-db (clj-redis/init))
+(defmacro wcr [& body] `(bench (-> clj-redis-db ~@body)))
 
 (defn bench-clj-redis
-  [{:keys [data-key data] db :clj-redis-pool :as opts}]
+  []
   (println "Benching clj-redis...")
   {:ping (wcr (clj-redis/ping))
    :set  (wcr (clj-redis/set data-key data))
    :get  (wcr (clj-redis/get data-key))})
 
+;;; Accession
+
+(def accession-spec (accession/connection-map))
 (defmacro wa [& body]
-  `(time-requests ~'opts (accession/with-connection ~'spec ~@body)))
+  `(bench (accession/with-connection accession-spec ~@body)))
 
 (defn bench-accession
-  [{:keys [data-key data] spec :accession-spec :as opts}]
+  []
   (println "Benching Accession...")
   {:ping (wa (accession/ping))
    :set  (wa (accession/set data-key data))
    :get  (wa (accession/get data-key))})
 
+;;; Carmine
+
+(def carmine-spec     (carmine/make-conn-spec))
+(defonce carmine-pool (carmine/make-conn-pool))
+
 (defmacro wc [& body]
-  `(time-requests ~'opts (carmine/with-conn ~'pool ~'spec ~@body)))
+  `(bench (carmine/with-conn carmine-pool carmine-spec ~@body)))
 
 (defn bench-carmine
-  [{:keys [data-key data] pool :carmine-pool spec :carmine-spec :as opts}]
+  []
   (println "Benching Carmine...")
   {:ping (wc (carmine/ping))
    :set  (wc (carmine/set data-key data))
    :get  (wc (carmine/get data-key))})
 
 (comment
-  ;; Define pools and stuff only ONCE
-  (def shared-opts {:carmine-spec   (carmine/make-conn-spec)
-                    :carmine-pool   (carmine/make-conn-pool)
-                    :accession-spec (accession/connection-map)
-                    :clj-redis-pool (clj-redis/init)})
 
-  (defn opts [& opts] (make-benching-options
-                       (merge shared-opts (apply hash-map opts))))
+  (bench-redis-clojure)
+  ;; {:ping 1548, :set 1742, :get 1732}
+  (bench-clj-redis)
+  ;; {:ping 498, :set 580, :get 557}
+  (bench-carmine)
+  ;; {:ping 642, :set 717, :get 718} ; Carmine 0.9.0
+  (bench-accession)
+  ;; {:ping 2214, :set "DNF", :get "DNF"} ; Doesn't close sockets!!
 
   ;; ./redis-benchmark -n 10000 -d 100 -c 1
   ;; PING-INLINE 1000 requests completed in 0.43 seconds
   ;; SET         1000 requests completed in 0.46 seconds
   ;; GET         1000 requests completed in 0.46 seconds
   ;; {:ping 430, :get 460, :set 460}
-
-  (bench-redis-clojure (opts :requests 10000 :clients 1 :data-size 100))
-  ;; {:ping 1548, :set 1742, :get 1732}
-  (bench-clj-redis     (opts :requests 10000 :clients 1 :data-size 100))
-  ;; {:ping 498, :set 580, :get 557}
-  (bench-carmine       (opts :requests 10000 :clients 1 :data-size 100))
-  ;; {:ping 642, :set 717, :get 718} ; Carmine 0.9.0
-  (bench-accession     (opts :requests 10000 :clients 1 :data-size 100))
-  ;; {:ping 2214, :set "DNF", :get "DNF"} ; Doesn't close sockets!!
 
   )
--- src/taoensso/carmine/utils.clj
+++ src/taoensso/carmine/utils.clj
@@ -10,3 +10,28 @@
                          `(do (in-ns '~(symbol ns))
                               (declare ~(symbol v))))) names)
          (in-ns '~(symbol orig-ns)))))
+
+(defmacro time-ns
+  "Returns number of nanoseconds it takes to execute body."
+  [& body]
+  `(let [t0# (System/nanoTime)]
+     ~@body
+     (- (System/nanoTime) t0#)))
+
+(defmacro bench
+  "Repeatedly executes form and returns time taken to complete execution."
+  [num-laps form & {:keys [warmup-laps num-threads as-ms?]
+                :or   {as-ms? true}}]
+  `(try (when ~warmup-laps (dotimes [_# ~warmup-laps] ~form))
+        (let [nanosecs#
+              (if-not ~num-threads
+                (time-ns (dotimes [_# ~num-laps] ~form))
+                (let [laps-per-thread# (int (/ ~num-laps ~num-threads))]
+                  (time-ns
+                   (->> (fn [] (future (dotimes [_# laps-per-thread#] ~form)))
+                        (repeatedly ~num-threads)
+                        doall
+                        (map deref)
+                        dorun))))]
+          (if ~as-ms? (Math/round (/ nanosecs# 1000000.0)) nanosecs#))
+        (catch Exception e# (str "DNF: " (.getMessage e#)))))
