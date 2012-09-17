b4062ef6d7aed837fb7319f04c9100b527ccfb95
Peter Taoussanis <ptaoussanis@gmail.com>
2012-09-17 15:17:33 +0700

    Bump version (major) (EXPERIMENTAL)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/carmine "0.10.4"
+(defproject com.taoensso/carmine "0.11.0-EXPERIMENTAL"
   :description "Simple, high-performance Redis (2.0+) client for Clojure."
   :url "https://github.com/ptaoussanis/carmine"
   :license {:name "Eclipse Public License"}
