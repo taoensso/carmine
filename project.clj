5b9b06b6b088a46d6f667dfa0c822247d633495a
Peter Taoussanis <ptaoussanis@gmail.com>
2012-11-05 00:20:59 +0700

    Bump dependencies (Timbre 1.0.0, Nippy 1.0.0)

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
                  [commons-pool/commons-pool   "1.6"]
                  [commons-codec/commons-codec "1.6"]
                  [org.clojure/data.json       "0.2.1"]
-                 [com.taoensso/timbre         "0.8.3"]
-                 [com.taoensso/nippy          "0.10.4"]]
+                 [com.taoensso/timbre         "1.0.0"]
+                 [com.taoensso/nippy          "1.0.0"]]
   :profiles {:1.3   {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4   {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5   {:dependencies [[org.clojure/clojure "1.5.0-alpha3"]]}
