1ba8607faed2afe5450ddbacc3b857c40b673774
Peter Taoussanis <ptaoussanis@gmail.com>
2013-03-15 20:00:40 +0700

    Bump version (1.7.0-beta2)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/carmine "1.7.0-beta1"
+(defproject com.taoensso/carmine "1.7.0-beta2"
   :description "Clojure Redis client & message queue"
   :url "https://github.com/ptaoussanis/carmine"
   :license {:name "Eclipse Public License"
