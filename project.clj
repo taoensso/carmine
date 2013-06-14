3bd70efe6191ddaaeea60e568474b9f4990c0c6a
Peter Taoussanis <ptaoussanis@gmail.com>
2013-06-14 23:20:57 +0700

    Clean up project.clj, add Expectations stuff

--- project.clj
+++ project.clj
@@ -8,17 +8,23 @@
                  [commons-pool/commons-pool   "1.6"]
                  [commons-codec/commons-codec "1.6"]
                  [org.clojure/data.json       "0.2.1"]
+                 [expectations                "1.4.43"]
                  [com.taoensso/timbre         "2.1.2"]
                  [com.taoensso/nippy          "1.2.1"]]
-  :profiles {:1.3  {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
+  :profiles {:1.4   {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5   {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :dev   {:dependencies [[ring/ring-core "1.1.0"]]}
              :test  {:dependencies [[ring/ring-core "1.1.0"]]}
              :bench {:dependencies [[org.clojars.tavisrudd/redis-clojure "1.3.1"]
                                     [clj-redis "0.0.12"]
                                     [accession "0.1.1"]]}}
-  :aliases {"test-all" ["with-profile" "test,1.4:test,1.5" "test"]}
-  :plugins [[codox "0.6.4"]]
+  :aliases {"test-all"    ["with-profile" "test,1.4:test,1.5" "do" "test,"
+                           "expectations"]
+            "test-auto"   ["with-profile" "test" "autoexpect"]
+            "start-dev"   ["with-profile" "dev,test,bench" "repl" ":headless"]
+            "start-bench" ["trampoline" "start-dev"]}
+  :plugins [[lein-expectations "0.0.7"]
+            [lein-autoexpect   "0.2.5"]
+            [codox             "0.6.4"]]
   :min-lein-version "2.0.0"
   :warn-on-reflection true)
