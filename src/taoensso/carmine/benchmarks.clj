c1f7ae0a935d10137256ceb9a9a9fa738860d556
Peter Taoussanis <ptaoussanis@gmail.com>
2013-06-24 20:07:07 +0700

    Perf: update benchmarks

--- src/taoensso/carmine/benchmarks.clj
+++ src/taoensso/carmine/benchmarks.clj
@@ -15,29 +15,34 @@
     (println)
     (println (str "Lap " (inc l) "/" laps "..."))
 
+    (when unpooled?
+      (println
+       {:wcar-unpooled  (bench* (wcar {:pool :none} "Do nothing"))
+        :ping-unpooled  (bench* (wcar {:pool :none} (car/ping)))}))
+
     (println
-     {:wcar-pooled    (bench* (wcar {}            "Do nothing"))
-      :wcar-unpooled  (when unpooled?
-                        (bench* (wcar {:pool :none} "Do nothing")))
-
-      :ping           (bench* (wcar {} (car/ping)))
-      :ping-unpooled  (when unpooled?
-                        (bench* (wcar {:pool :none} (car/ping))))
-      :ping-pipelined (bench* (wcar {} (dorun (repeatedly 100 car/ping))))
-
-      :set            (bench* (wcar {} (car/set bench-key bench-data)))
-      :get            (bench* (wcar {} (car/get bench-key)))
-      :roundtrip      (bench* (wcar {} (car/ping)
-                                       (car/set bench-key bench-data)
-                                       (car/get bench-key)))}))
+     {:wcar (bench* (wcar {} "Do nothing"))
+      :ping (bench* (wcar {} (car/ping)))
+      :set  (bench* (wcar {} (car/set bench-key bench-data)))
+      :get  (bench* (wcar {} (car/get bench-key)))
+      :roundtrip (bench* (wcar {} (car/ping)
+                                  (car/set bench-key bench-data)
+                                  (car/get bench-key)))
+      :ping-pipelined (bench* (wcar {} (dorun (repeatedly 100 car/ping))))}))
 
   (println)
   (println "Done! (Time for cake?)")
   true)
 
-(comment (bench {}))
+(comment (bench {:unpooled? true})
+         (bench {}))
 
 (comment
+
+  ;;; 24 June 2013, + Perf. refactoring
+  ;; {:wcar-unpooled 53188, :ping-unpooled 33540}
+  ;; {:wcar 42, :ping 576, :set 608, :get 596, :roundtrip 875, :ping-pipelined 13939}
+  ;; (/ (* 10000 100) 14.0) => ~ 71k pings/sec
 
   ;;; 24 June 2013: Clojure 1.5.1, JVM 7, Carmine 2.0.0-alpha4
   ;; {:wcar-pooled 191, :wcar-unpooled 29991, :ping 1532, :ping-unpooled 29589,
