23916f8f09e52848cdbfd543406a1e6136627418
Peter Taoussanis <ptaoussanis@gmail.com>
2013-06-26 14:41:48 +0700

    Add `before-run`, `after-run` Expectations fns

--- test/taoensso/carmine/tests/main.clj
+++ test/taoensso/carmine/tests/main.clj
@@ -3,4 +3,7 @@
             [taoensso.carmine :as car  :refer (wcar)]
             [taoensso.carmine.benchmarks :as benchmarks]))
 
+(defn- before-run {:expectations-options :before-run} [])
+(defn- after-run  {:expectations-options :after-run}  [])
+
 (expect (benchmarks/bench {}))
