8051d7d8ed5c9df061bdd6129bdc8ee7013cd521
Peter Taoussanis <ptaoussanis@gmail.com>
2013-06-26 17:03:40 +0700

    Add `codox` lein alias

--- project.clj
+++ project.clj
@@ -15,13 +15,15 @@
              :1.5   {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6   {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :dev   {:dependencies []}
-             :test  {:dependencies [[com.taoensso/faraday "0.10.1"]]}
+             :test  {:dependencies [[ring/ring-core       "1.2.0-RC1"]
+                                    [com.taoensso/faraday "0.10.1"]]}
              :bench {:dependencies [] :jvm-opts ["-server"]}}
   :aliases {"test-all"    ["with-profile" "test,1.4:test,1.5:test,1.6"
                            "do" "test," "expectations"]
             "test-auto"   ["with-profile" "test" "autoexpect"]
             "start-dev"   ["with-profile" "dev,test,bench" "repl" ":headless"]
-            "start-bench" ["trampoline" "start-dev"]}
+            "start-bench" ["trampoline" "start-dev"]
+            "codox"       ["with-profile" "test,1.5" "doc"]}
   :plugins [[lein-expectations "0.0.7"]
             [lein-autoexpect   "0.2.5"]
             [codox             "0.6.4"]]
