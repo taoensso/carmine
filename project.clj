f96eca4a6a6819788ee41959b044d99a72722aba
Peter Taoussanis <ptaoussanis@gmail.com>
2013-11-21 17:53:24 +0700

    v2.5.0-alpha1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/carmine "2.4.0"
+(defproject com.taoensso/carmine "2.5.0-alpha1"
   :description "Clojure Redis client & message queue"
   :url "https://github.com/ptaoussanis/carmine"
   :license {:name "Eclipse Public License"
