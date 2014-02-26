013d96b2070f4bec2175ff66e89d64057e36d46d
Peter Taoussanis <ptaoussanis@gmail.com>
2014-02-26 16:27:58 +0700

    Fix project.clj to prevent unnecessary downstream deps

--- project.clj
+++ project.clj
@@ -11,16 +11,16 @@
                 *assert* true}
   :dependencies
   [[org.clojure/clojure         "1.4.0"]
-   [org.clojure/data.json       "0.2.4"]
-   [com.taoensso/encore         "0.8.0"]
-   [com.taoensso/timbre         "3.0.0"]
-   [com.taoensso/nippy          "2.6.0-alpha3"]
+   [com.taoensso/encore         "0.9.2"]
+   [com.taoensso/timbre         "3.1.0"]
+   [com.taoensso/nippy          "2.6.0-beta2"]
    [commons-pool/commons-pool   "1.6"]
-   [commons-codec/commons-codec "1.9"]]
+   [commons-codec/commons-codec "1.9"]
+   [org.clojure/data.json       "0.2.4"]]
 
   :test-paths ["test" "src"]
   :profiles
-  {:build {:hooks ^:replace []} ; Workaround to avoid :dev hooks during deploy
+  {;; :default [:base :system :user :provided :dev]
    :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
    :1.6  {:dependencies [[org.clojure/clojure "1.6.0-beta1"]]}
    :test {:dependencies [[expectations            "1.4.56"]
@@ -30,23 +30,26 @@
                          [ring/ring-core          "1.2.1"]]
           :plugins [[lein-expectations "0.0.8"]
                     [lein-autoexpect   "1.2.2"]]}
+   :dev* [:dev {:jvm-opts ^:replace ["-server"]
+                ;; :hooks [cljx.hooks leiningen.cljsbuild] ; cljx
+                }]
    :dev
    [:1.6 :test
-    {:jvm-opts ^:replace ["-server"]
-     :hooks []
-     :dependencies []
+    {:dependencies []
      :plugins []}]}
 
   :plugins [[lein-ancient "0.5.4"]
             [codox        "0.6.7"]]
 
-  ;; :codox {:sources ["target/classes"]} ; For use with cljx
+  ;; :codox {:sources ["target/classes"]} ; cljx
   :aliases
-  {"test-all"   ["with-profile" "+test:+1.5,+test:+1.6,+test" "expectations"]
+  {"test-all"   ["with-profile" "default:+1.5:+1.6" "expectations"]
+   ;; "test-all"   ["with-profile" "default:+1.6" "expectations"]
    "test-auto"  ["with-profile" "+test" "autoexpect"]
-   "start-dev"  ["with-profile" "+dev" "repl" ":headless"]
-   "codox"      ["with-profile" "+test" "doc"]
-   "deploy-lib" ["with-profile" "+dev,+build" "do" "deploy" "clojars," "install"]}
+   ;; "build-once" ["do" "cljx" "once," "cljsbuild" "once"] ; cljx
+   ;; "deploy-lib" ["do" "build-once," "deploy" "clojars," "install"] ; cljx
+   "deploy-lib" ["do" "deploy" "clojars," "install"]
+   "start-dev"  ["with-profile" "+dev*" "repl" ":headless"]}
 
   :repositories
   {"sonatype"
