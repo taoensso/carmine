886e7876b69d7d2d4fd66b9b862d74e87d2d6c7a
Peter Taoussanis <ptaoussanis@gmail.com>
2014-07-06 14:22:14 +0700

    Bump deps

--- project.clj
+++ project.clj
@@ -12,29 +12,29 @@
 
   :dependencies
   [[org.clojure/clojure         "1.4.0"]
-   [com.taoensso/encore         "1.4.0"]
-   [com.taoensso/timbre         "3.1.6"]
+   [com.taoensso/encore         "1.7.0"]
+   [com.taoensso/timbre         "3.2.1"]
    [com.taoensso/nippy          "2.6.3"]
    [org.apache.commons/commons-pool2 "2.2"]
    [commons-codec/commons-codec "1.9"]
-   [org.clojure/data.json       "0.2.4"]]
+   [org.clojure/data.json       "0.2.5"]]
 
   :profiles
   {;; :default [:base :system :user :provided :dev]
    :server-jvm {:jvm-opts ^:replace ["-server"]}
    :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
    :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
-   :test {:dependencies [[expectations            "1.4.56"]
-                         [org.clojure/test.check  "0.5.7"]
-                         [com.taoensso/faraday    "1.3.0"]
-                         [clj-aws-s3              "0.3.8"]
-                         [ring/ring-core          "1.2.2"]]
+   :test {:dependencies [[expectations            "2.0.7"]
+                         [org.clojure/test.check  "0.5.8"]
+                         [com.taoensso/faraday    "1.4.0"]
+                         [clj-aws-s3              "0.3.9"]
+                         [ring/ring-core          "1.3.0"]]
           :plugins [[lein-expectations "0.0.8"]
                     [lein-autoexpect   "1.2.2"]]}
    :dev
    [:1.6 :test
     {:plugins [[lein-ancient "0.5.4"]
-               [codox        "0.6.7"]]}]}
+               [codox        "0.8.10"]]}]}
 
   :test-paths ["test" "src"]
 
