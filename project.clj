c53e2c9c97d854db7ecac8db234499f6c9a56e7b
Peter Taoussanis <ptaoussanis@gmail.com>
2014-08-27 18:59:50 +0700

    Bump deps

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
 
   :dependencies
   [[org.clojure/clojure         "1.4.0"]
-   [com.taoensso/encore         "1.7.0"]
+   [com.taoensso/encore         "1.7.1"]
    [com.taoensso/timbre         "3.2.1"]
    [com.taoensso/nippy          "2.6.3"]
    [org.apache.commons/commons-pool2 "2.2"]
@@ -24,11 +24,11 @@
    :server-jvm {:jvm-opts ^:replace ["-server"]}
    :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
    :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
-   :test {:dependencies [[expectations            "2.0.7"]
-                         [org.clojure/test.check  "0.5.8"]
-                         [com.taoensso/faraday    "1.4.0"]
+   :test {:dependencies [[expectations            "2.0.9"]
+                         [org.clojure/test.check  "0.5.9"]
+                         [com.taoensso/faraday    "1.5.0"]
                          [clj-aws-s3              "0.3.9"]
-                         [ring/ring-core          "1.3.0"]]
+                         [ring/ring-core          "1.3.1"]]
           :plugins [[lein-expectations "0.0.8"]
                     [lein-autoexpect   "1.2.2"]]}
    :dev
