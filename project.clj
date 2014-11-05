bdda0e4b96ac118af360ddde88a772cb953979c0
Peter Taoussanis <ptaoussanis@gmail.com>
2014-11-05 16:44:36 +0700

    v2.8.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/carmine "2.7.1"
+(defproject com.taoensso/carmine "2.8.0-SNAPSHOT"
   :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Clojure Redis client & message queue"
   :url "https://github.com/ptaoussanis/carmine"
