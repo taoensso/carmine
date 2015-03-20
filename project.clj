bb95f38d54a53ab2e56bbd345e98c59a3dbe0e90
Peter Taoussanis <ptaoussanis@gmail.com>
2015-03-20 15:04:38 +0700

    Bump deps

--- project.clj
+++ project.clj
@@ -12,19 +12,19 @@
 
   :dependencies
   [[org.clojure/clojure              "1.4.0"]
-   [com.taoensso/encore              "1.21.0"]
+   [com.taoensso/encore              "1.22.0"]
    [com.taoensso/timbre              "3.4.0"]
    [com.taoensso/nippy               "2.8.0"]
    [org.apache.commons/commons-pool2 "2.3"]
    [commons-codec/commons-codec      "1.10"]
-   [org.clojure/data.json            "0.2.5"]]
+   [org.clojure/data.json            "0.2.6"]]
 
   :profiles
   {;; :default [:base :system :user :provided :dev]
    :server-jvm {:jvm-opts ^:replace ["-server"]}
    :1.5  {:dependencies [[org.clojure/clojure     "1.5.1"]]}
    :1.6  {:dependencies [[org.clojure/clojure     "1.6.0"]]}
-   :test {:dependencies [[expectations            "2.0.16"]
+   :test {:dependencies [[expectations            "2.1.0"]
                          [org.clojure/test.check  "0.7.0"]
                          [com.taoensso/faraday    "1.5.0"]
                          [clj-aws-s3              "0.3.10"]
@@ -33,8 +33,8 @@
                     [lein-autoexpect   "1.4.2"]]}
    :dev
    [:1.6 :test
-    {:plugins [[lein-ancient "0.5.4"]
-               [codox        "0.8.10"]]}]}
+    {:plugins [[lein-ancient "0.6.4"]
+               [codox        "0.8.11"]]}]}
 
   :test-paths ["test" "src"]
 
