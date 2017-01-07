taoensso/carmine

commit 1ec18fe8963b88229eddca4728166a011189e66e

Refactor command defs
This was long-overdue for a refactor. In particular, removing the
compile-time json parsing speeds startup and lets us eliminate a
dependency.

Still *lots* of other low-hanging fruit here + elsewhere, just
not a priority right now.

src/taoensso/carmine.clj
@@ -125,9 +125,7 @@
 
 ;;;; Standard commands
 
-;; This'll define an appropriate fn (with arglists + docstring) for each of
-;; Redis' 190+ commands as specified in the official `commands.json` file
-(commands/defcommands)
+(commands/defcommands) ; Defines 190+ Redis command fns as per official spec
 
 (defn redis-call
   "Sends low-level requests to Redis. Useful for DSLs, certain kinds of command

project.clj
@@ -16,8 +16,7 @@
    [com.taoensso/timbre              "4.7.3"]
    [com.taoensso/nippy               "2.12.0"]
    [org.apache.commons/commons-pool2 "2.4.2"]
-   [commons-codec/commons-codec      "1.10"]
-   [org.clojure/data.json            "0.2.6"]]
+   [commons-codec/commons-codec      "1.10"]]
 
   :profiles
   {;; :default [:base :system :user :provided :dev]
@@ -38,8 +37,10 @@
                     [lein-autoexpect   "1.9.0"]]}
    :dev
    [:1.9 :test :server-jvm
-    {:plugins [[lein-ancient "0.6.10"]
-               [lein-codox   "0.9.5"]]}]}
+    {:dependencies [[org.clojure/data.json "0.2.6"]]
+     :plugins
+     [[lein-ancient "0.6.10"]
+      [lein-codox   "0.9.5"]]}]}
 
   :test-paths ["test" "src"]
 

src/commands/commands.list
@@ -1,199 +1,0 @@
-197 commands in refspec:
----
-append - [key value]
-auth - [password]
-bgrewriteaof - []
-bgsave - []
-bitcount - [key & args]
-bitfield - [key & args]
-bitop - [operation destkey key & args]
-bitpos - [key bit & args]
-blpop - [key & args]
-brpop - [key & args]
-brpoplpush - [source destination timeout]
-client getname - []
-client kill - [& args]
-client list - []
-client pause - [timeout]
-client reply - [reply-mode]
-client setname - [connection-name]
-cluster addslots - [slot & args]
-cluster count-failure-reports - [node-id]
-cluster countkeysinslot - [slot]
-cluster delslots - [slot & args]
-cluster failover - [& args]
-cluster forget - [node-id]
-cluster getkeysinslot - [slot count]
-cluster info - []
-cluster keyslot - [key]
-cluster meet - [ip port]
-cluster nodes - []
-cluster replicate - [node-id]
-cluster reset - [& args]
-cluster saveconfig - []
-cluster set-config-epoch - [config-epoch]
-cluster setslot - [slot subcommand & args]
-cluster slaves - [node-id]
-cluster slots - []
-command - []
-command count - []
-command getkeys - []
-command info - [command-name & args]
-config get - [parameter]
-config resetstat - []
-config rewrite - []
-config set - [parameter value]
-dbsize - []
-debug object - [key]
-debug segfault - []
-decr - [key]
-decrby - [key decrement]
-del - [key & args]
-discard - []
-dump - [key]
-echo - [message]
-eval - [script numkeys key & args]
-evalsha - [sha1 numkeys key & args]
-exec - []
-exists - [key & args]
-expire - [key seconds]
-expireat - [key timestamp]
-flushall - []
-flushdb - []
-geoadd - [key longitude latitude member & args]
-geodist - [key member1 member2 & args]
-geohash - [key member & args]
-geopos - [key member & args]
-georadius - [key longitude latitude radius unit & args]
-georadiusbymember - [key member radius unit & args]
-get - [key]
-getbit - [key offset]
-getrange - [key start end]
-getset - [key value]
-hdel - [key field & args]
-hexists - [key field]
-hget - [key field]
-hgetall - [key]
-hincrby - [key field increment]
-hincrbyfloat - [key field increment]
-hkeys - [key]
-hlen - [key]
-hmget - [key field & args]
-hmset - [key field value & args]
-hscan - [key cursor & args]
-hset - [key field value]
-hsetnx - [key field value]
-hstrlen - [key field]
-hvals - [key]
-incr - [key]
-incrby - [key increment]
-incrbyfloat - [key increment]
-info - [& args]
-keys - [pattern]
-lastsave - []
-lindex - [key index]
-linsert - [key where pivot value]
-llen - [key]
-lpop - [key]
-lpush - [key value & args]
-lpushx - [key value]
-lrange - [key start stop]
-lrem - [key count value]
-lset - [key index value]
-ltrim - [key start stop]
-mget - [key & args]
-migrate - [host port key destination-db timeout & args]
-monitor - []
-move - [key db]
-mset - [key value & args]
-msetnx - [key value & args]
-multi - []
-object - [subcommand & args]
-persist - [key]
-pexpire - [key milliseconds]
-pexpireat - [key milliseconds-timestamp]
-pfadd - [key element & args]
-pfcount - [key & args]
-pfmerge - [destkey sourcekey & args]
-ping - [& args]
-psetex - [key milliseconds value]
-psubscribe - [pattern & args]
-pttl - [key]
-publish - [channel message]
-pubsub - [subcommand & args]
-punsubscribe - [& args]
-quit - []
-randomkey - []
-readonly - []
-readwrite - []
-rename - [key newkey]
-renamenx - [key newkey]
-restore - [key ttl serialized-value & args]
-role - []
-rpop - [key]
-rpoplpush - [source destination]
-rpush - [key value & args]
-rpushx - [key value]
-sadd - [key member & args]
-save - []
-scan - [cursor & args]
-scard - [key]
-script debug - [mode]
-script exists - [script & args]
-script flush - []
-script kill - []
-script load - [script]
-sdiff - [key & args]
-sdiffstore - [destination key & args]
-select - [index]
-set - [key value & args]
-setbit - [key offset value]
-setex - [key seconds value]
-setnx - [key value]
-setrange - [key offset value]
-shutdown - [& args]
-sinter - [key & args]
-sinterstore - [destination key & args]
-sismember - [key member]
-slaveof - [host port]
-slowlog - [subcommand & args]
-smembers - [key]
-smove - [source destination member]
-sort - [key & args]
-spop - [key & args]
-srandmember - [key & args]
-srem - [key member & args]
-sscan - [key cursor & args]
-strlen - [key]
-subscribe - [channel & args]
-sunion - [key & args]
-sunionstore - [destination key & args]
-sync - []
-time - []
-ttl - [key]
-type - [key]
-unsubscribe - [& args]
-unwatch - []
-wait - [numslaves timeout]
-watch - [key & args]
-zadd - [key & args]
-zcard - [key]
-zcount - [key min max]
-zincrby - [key increment member]
-zinterstore - [destination numkeys key & args]
-zlexcount - [key min max]
-zrange - [key start stop & args]
-zrangebylex - [key min max & args]
-zrangebyscore - [key min max & args]
-zrank - [key member]
-zrem - [key member & args]
-zremrangebylex - [key min max]
-zremrangebyrank - [key start stop]
-zremrangebyscore - [key min max]
-zrevrange - [key start stop & args]
-zrevrangebylex - [key max min & args]
-zrevrangebyscore - [key max min & args]
-zrevrank - [key member]
-zscan - [key cursor & args]
-zscore - [key member]
-zunionstore - [destination numkeys key & args]

src/taoensso/carmine/cluster.clj
@@ -1,96 +1,0 @@
-(ns taoensso.carmine.cluster
-  "EXPERIMENTAL support for Redis Cluster atop Carmine."
-  {:author "Ben Poweski"}
-  (:require [clojure.string   :as str]
-            [taoensso.encore  :as enc]
-            [taoensso.carmine :as car]
-            [taoensso.carmine
-             (utils       :as utils)
-             (protocol    :as protocol)
-             (connections :as conns)
-             (commands    :as commands)]))
-
-;; TODO Migrate to new design
-
-;;; Description of new design:
-;; The new design should be significantly more flexible + performant for use
-;; with Cluster:
-;; * `protocol/*context*` now contains a request queue (atom []).
-;; * Redis commands previously wrote directly to io buffer, they now push
-;;   'requests' to this queue instead.
-;; * A 'request' looks like ["GET" "my-key" "my-val"] and has optional
-;;   metadata which includes `:expected-keyslot` - hashed cluster key (crc16).
-;;
-;; * Request pushing + metadata is all handled by `commands/enqueue-request`.
-;;
-;; * Before actually fetching server replies, all queued requests are written to
-;;   io buffer with `protocol/execute-requests`.
-;;
-;; * This fn works with dynamic arguments (default), or with an explicit
-;;   Connection and requests. It is fast + flexible (gives us a lot of room to
-;;   make Cluster-specific adjustments).
-
-;;; Sketch of suggested implementation:
-;; * `:cluster` should be provided as part of connection :spec options.
-;; * The `protocol/execute-requests` fn could be modded so that when the
-;;   :cluster key is present, behaviour is delegated to
-;;   `cluster/execute-requests`.
-;; * This fn groups requests by the (cached) keyslot->server info:
-;;   (let [plan (group-by <grouping-fn> requests)]
-;;    <...>).
-;; * Futures can then call `protocol/execute-requests` with the explicit
-;;   expected connections + planned requests & deliver replies to promises.
-;; * All Cluster replies could be inspected for Cluster errors like MOVE, etc.
-;;   This is easier than before since exceptions now have
-;;   `(ex-data <ex>)` with a :prefix key that'll be :moved, :ack, :wrongtype, etc.
-;; * After all promises have returned, we could regroup for any moved keys and
-;;   loop, continuing to accumulate appropriate replies.
-;; * We eventually return all the replies in the same order they were provided.
-;;   Parsers & other goodies will just work as expected since all that info is
-;;   attached to the requests themselves.
-
-(comment ; Example
-
-  ;; Step 1:
-  (wcar {:cluster "foo"}
-    (car/get "key-a")
-    (car/get "key-b")
-    (car/get "key-c"))
-
-  ;; Step 2:
-  ;; protocol/execute-requests will receive requests as:
-  ;; [["GET" "key-a"] ["GET" "key-b"] ["GET" "key-c"]]
-  ;; Each will have :expected-keyslot metadata.
-  ;; None have :parser metadata in this case, but it wouldn't make a difference
-  ;; to our handling here.
-
-  ;; Step 3:
-  ;; Does our cache know which servers serve each of the above slots?[1]
-  ;; Group requests per server + `execute-requests` in parallel with
-  ;; appropriate Connections specified (will override the dynamic args).
-
-  ;; Step 4:
-  ;; Wait for all promises to be fulfilled (a timeout may be sensible).
-
-  ;; Step 5:
-  ;; Identify (with `(:prefix (ex-data <ex>))`) which replies are Cluster/timeout
-  ;; errors that imply we should try again.
-
-  ;; Step 6:
-  ;; Continue looping like this until we've got all expected replies or we're
-  ;; giving up for certain replies.
-
-  ;; Step 7:
-  ;; Return all replies in order as a single vector (i.e. the consumer won't be
-  ;; aware which nodes served which replies).
-
-  ;; [1]
-  ;; Since slots are distributed to servers in _ranges_, we can do this quite
-  ;; efficiently.
-  ;; Let's say we request a key at slot 42 and determine that it's at
-  ;; 127.0.0.1:6379 so we cache {42 <server 127.0.0.1:6379>}.
-  ;;
-  ;; Now we want a key at slot 78 but we have no idea where it is. We can scan
-  ;; our cache and find the nearest known slot to 78 and use that for our first
-  ;; attempt. So with n nodes, we'll have at most n-1 expected-slot misses.
-  )

commit b6dd3ec96cc6011b47646a225d31859a886544d5

[#189] Hotfix: `redis-call` was broken by 1ec18fe

src/taoensso/carmine.clj
@@ -138,7 +138,7 @@
     (fn [[cmd & args]]
       (let [cmd-parts (-> cmd name str/upper-case (str/split #"-"))
             request   (into (vec cmd-parts) args)]
-        (commands/enqueue-request request (count cmd-parts))))
+        (commands/enqueue-request (count cmd-parts) request)))
     requests))
 
 (comment (wcar {} (redis-call [:set "foo" "bar"] [:get "foo"]