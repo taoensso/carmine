b51cc76aca6747918ce2bb87b34ee0c2f3299c81
Peter Taoussanis <ptaoussanis@gmail.com>
2017-03-24 12:36:45 +0100

    Bump deps

--- project.clj
+++ project.clj
@@ -12,9 +12,9 @@
 
   :dependencies
   [[org.clojure/clojure              "1.5.1"]
-   [com.taoensso/encore              "2.86.1"]
-   [com.taoensso/timbre              "4.7.3"]
-   [com.taoensso/nippy               "2.12.2"]
+   [com.taoensso/encore              "2.90.1"]
+   [com.taoensso/timbre              "4.8.0"]
+   [com.taoensso/nippy               "2.13.0"]
    [org.apache.commons/commons-pool2 "2.4.2"]
    [commons-codec/commons-codec      "1.10"]]
 
@@ -32,7 +32,7 @@
                          [org.clojure/test.check  "0.9.0"]
                          [com.taoensso/faraday    "1.9.0"]
                          [clj-aws-s3              "0.3.10"]
-                         [ring/ring-core          "1.5.0"]]
+                         [ring/ring-core          "1.5.1"]]
           :plugins [[lein-expectations "0.0.8"]
                     [lein-autoexpect   "1.9.0"]]}
    :dev
@@ -40,7 +40,7 @@
     {:dependencies [[org.clojure/data.json "0.2.6"]]
      :plugins
      [[lein-ancient "0.6.10"]
-      [lein-codox   "0.10.1"]]}]}
+      [lein-codox   "0.10.3"]]}]}
 
   :test-paths ["test" "src"]
 
