75e762db2df82e18203d92194ba5f449b4b9940f
Peter Taoussanis <ptaoussanis@gmail.com>
2018-11-03 13:02:27 +0100

    [Experimental] Add `:swap/delete` support to swap utils

--- src/lua/cas-hset.lua
+++ src/lua/cas-hset.lua
@@ -1,18 +1,20 @@
 local curr_val = redis.call('hget', _:k, _:field);
 
+local do_swap = false;
 if (_:old-?sha ~= '') then
    local curr_sha = redis.sha1hex(curr_val);
-   if (curr_sha == _:old-?sha) then
-      redis.call('hset', _:k, _:field, _:new-val);
-      return 1;
-   else
-      return 0;
-   end
+   if (curr_sha == _:old-?sha) then do_swap = true; end
 else
-   if (curr_val == _:old-?val) then
-      redis.call('hset', _:k, _:field, _:new-val);
-      return 1;
+   if (curr_val == _:old-?val) then do_swap = true; end
+end
+
+if do_swap then
+   if (_:delete == '1') then
+      redis.call('hdel', _:k, _:field);
    else
-      return 0;
+      redis.call('hset', _:k, _:field, _:new-val);
    end
+   return 1;
+else
+   return 0;
 end
