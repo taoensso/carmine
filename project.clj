58d68a6793709ba35088c6342b395a56739ef432
Peter Taoussanis <peter@taoensso.com>
2023-03-16 14:39:09 +0100

    [nop] Update project template

--- project.clj
+++ project.clj
@@ -2,11 +2,12 @@
   :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Clojure Redis client & message queue"
   :url "https://github.com/ptaoussanis/carmine"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"
-            :distribution :repo
-            :comments "Same as Clojure"}
   :min-lein-version "2.3.3"
+
+  :license
+  {:name "Eclipse Public License 1.0"
+   :url  "http://www.eclipse.org/legal/epl-v10.html"}
+
   :global-vars
   {*warn-on-reflection* true
    *assert*             true
@@ -24,15 +25,19 @@
    [lein-ancient "0.7.0"]
    [lein-codox   "0.10.8"]]
 
+  :codox
+  {:language #{:clojure #_:clojurescript}
+   :base-language :clojure}
+
   :profiles
   {;; :default [:base :system :user :provided :dev]
+   :depr       {:jvm-opts ["-Dtaoensso.elide-deprecated=true"]}
    :server-jvm {:jvm-opts ^:replace ["-server"]}
-   :provided {:dependencies [[org.clojure/clojure "1.11.1"]]}
-   :c1.11    {:dependencies [[org.clojure/clojure "1.11.1"]]}
-   :c1.10    {:dependencies [[org.clojure/clojure "1.10.1"]]}
-   :c1.9     {:dependencies [[org.clojure/clojure "1.9.0"]]}
 
-   :depr     {:jvm-opts ["-Dtaoensso.elide-deprecated=true"]}
+   :provided {:dependencies [[org.clojure/clojure    "1.11.1"]]}
+   :c1.11    {:dependencies [[org.clojure/clojure    "1.11.1"]]}
+   :c1.10    {:dependencies [[org.clojure/clojure    "1.10.1"]]}
+   :c1.9     {:dependencies [[org.clojure/clojure    "1.9.0"]]}
    :test     {:dependencies [[org.clojure/test.check "1.1.1"]]}
    :dev
    [:c1.11 :test :server-jvm :depr
@@ -40,17 +45,24 @@
      [[org.clojure/data.json "2.4.0"]
       [com.taoensso/faraday  "1.11.4"]
       [clj-aws-s3            "0.3.10"]
-      [ring/ring-core        "1.9.6"]]}]}
+      [ring/ring-core        "1.9.6"]]}]
+
+   :graal-tests
+   {:dependencies [[org.clojure/clojure "1.11.1"]
+                   [com.github.clj-easy/graal-build-time "0.1.4"]]
+    :main taoensso.graal-tests
+    :aot [taoensso.graal-tests]
+    :uberjar-name "graal-tests.jar"}}
 
   :test-paths ["test" #_"src"]
 
   :aliases
   {"start-dev"  ["with-profile" "+dev" "repl" ":headless"]
+   ;; "build-once" ["do" ["clean"] "cljsbuild" "once"]
    "deploy-lib" ["do" #_["build-once"] ["deploy" "clojars"] ["install"]]
 
-   "test-all"
-   ["do" ["clean"]
-    "with-profile" "+c1.11:+c1.10:+c1.9" "test"]}
+   "test-clj"   ["with-profile" "+c1.11:+c1.10:+c1.9" "test"]
+   "test-all"   ["do" ["clean"] "test-clj" #_"test-cljs"]}
 
   :repositories
   {"sonatype-oss-public"
---
+++ test/taoensso/graal_tests.clj
@@ -0,0 +1,5 @@
+(ns taoensso.graal-tests
+  (:require [taoensso.carmine :as car])
+  (:gen-class))
+
+(defn -main [& args] (println "Namespace loaded successfully"))
