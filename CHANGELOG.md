c624e999e85bdeb5b9c5286eddaa746c3f8bfa0f
Peter Taoussanis <peter@taoensso.com>
2023-07-18 12:01:19 +0200

    [nop] Update project template

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-This project uses [Break Versioning](https://github.com/ptaoussanis/encore/blob/master/BREAK-VERSIONING.md).
+ This project uses Break Versioning (https://www.taoensso.com/break-versioning)
 
 ## `3.3.0-RC1` (2023-07-18)
 
--- project.clj
+++ project.clj
@@ -1,17 +1,11 @@
 (defproject com.taoensso/carmine "3.3.0-RC1"
   :author "Peter Taoussanis <https://www.taoensso.com>"
-  :description "Clojure Redis client & message queue"
-  :url "https://github.com/ptaoussanis/carmine"
-  :min-lein-version "2.3.3"
+  :description "Redis client + message queue for Clojure"
+  :url "https://github.com/taoensso/carmine"
 
   :license
-  {:name "Eclipse Public License 1.0"
-   :url  "http://www.eclipse.org/legal/epl-v10.html"}
-
-  :global-vars
-  {*warn-on-reflection* true
-   *assert*             true
-   *unchecked-math*     false #_:warn-on-boxed}
+  {:name "Eclipse Public License - v 1.0"
+   :url  "https://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies
   [[com.taoensso/encore              "3.62.1"]
@@ -21,50 +15,55 @@
    [org.apache.commons/commons-pool2 "2.11.1"]
    [commons-codec/commons-codec      "1.16.0"]]
 
-  :plugins
-  [[lein-pprint  "1.3.2"]
-   [lein-ancient "0.7.0"]
-   [com.taoensso.forks/lein-codox "0.10.9"]]
-
-  :codox
-  {:language #{:clojure #_:clojurescript}
-   :base-language :clojure}
-
   :profiles
   {;; :default [:base :system :user :provided :dev]
-   :depr       {:jvm-opts ["-Dtaoensso.elide-deprecated=true"]}
-   :server-jvm {:jvm-opts ^:replace ["-server"]}
-
    :provided {:dependencies [[org.clojure/clojure    "1.11.1"]]}
    :c1.11    {:dependencies [[org.clojure/clojure    "1.11.1"]]}
    :c1.10    {:dependencies [[org.clojure/clojure    "1.10.1"]]}
    :c1.9     {:dependencies [[org.clojure/clojure    "1.9.0"]]}
-   :test     {:dependencies [[org.clojure/test.check "1.1.1"]]}
-   :dev
-   [:c1.11 :test :server-jvm :depr
-    {:dependencies
-     [[org.clojure/data.json "2.4.0"]
-      [com.taoensso/faraday  "1.12.0"]
-      [clj-aws-s3            "0.3.10"]
-      [ring/ring-core        "1.10.0"]]}]
+
+   :test
+   {:jvm-opts ["-Dtaoensso.elide-deprecated=true"]
+    :global-vars
+    {*warn-on-reflection* true
+     *assert*             true
+     *unchecked-math*     false #_:warn-on-boxed}
+
+    :dependencies
+    [[org.clojure/test.check "1.1.1"]]}
 
    :graal-tests
    {:dependencies [[org.clojure/clojure "1.11.1"]
                    [com.github.clj-easy/graal-build-time "0.1.4"]]
     :main taoensso.graal-tests
     :aot [taoensso.graal-tests]
-    :uberjar-name "graal-tests.jar"}}
+    :uberjar-name "graal-tests.jar"}
+
+   :dev
+   [:c1.11 :test
+    {:jvm-opts ["-server"]
+     :dependencies
+     [[org.clojure/data.json "2.4.0"]
+      [com.taoensso/faraday  "1.12.0"]
+      [clj-aws-s3            "0.3.10"]
+      [ring/ring-core        "1.10.0"]]
+
+     :plugins
+     [[lein-pprint  "1.3.2"]
+      [lein-ancient "0.7.0"]
+      [com.taoensso.forks/lein-codox "0.10.10"]]
+
+     :codox
+     {:language #{:clojure #_:clojurescript}
+      :base-language :clojure}}]}
 
   :test-paths ["test" #_"src"]
 
   :aliases
-  {"start-dev"  ["with-profile" "+dev" "repl" ":headless"]
-   ;; "build-once" ["do" ["clean"] "cljsbuild" "once"]
-   "deploy-lib" ["do" #_["build-once"] ["deploy" "clojars"] ["install"]]
+  {"start-dev"     ["with-profile" "+dev" "repl" ":headless"]
+   ;; "build-once" ["do" ["clean"] ["cljsbuild" "once"]]
+   "deploy-lib"    ["do" #_["build-once"] ["deploy" "clojars"] ["install"]]
 
-   "test-clj"   ["with-profile" "+c1.11:+c1.10:+c1.9" "test"]
-   "test-all"   ["do" ["clean"] "test-clj" #_"test-cljs"]}
-
-  :repositories
-  {"sonatype-oss-public"
-   "https://oss.sonatype.org/content/groups/public/"})
+   "test-clj"     ["with-profile" "+c1.11:+c1.10:+c1.9" "test"]
+   ;; "test-cljs" ["with-profile" "+test" "cljsbuild"   "test"]
+   "test-all"     ["do" ["clean"] ["test-clj"] #_["test-cljs"]]})
