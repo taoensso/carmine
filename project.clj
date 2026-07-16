538bc1825c30083a325b37832fe16553655c812a
Peter Taoussanis <peter@taoensso.com>
2026-07-16 17:04:48 +0700

    [fix] Update deps, add Clojure 1.13 alpha, harden CI
    - Update dependencies and add Clojure 1.13 alpha to the test profiles.
    
    - Harden GitHub Actions with least-privilege permissions, scoped branch
      triggers, superseded-run cancellation, SHA-pinned actions maintained
      by Dependabot, pinned build tools, and job-aware dependency caches.
    
    - Correct the Graal dependency cache to hash `project.clj`, which
      actually defines this project's dependencies.
    
    - Make `setup-java` check for the latest patch of each requested Java
      major. This avoids stale runner-cached JDK 17 builds affected by
      JDK-8287073:
      https://bugs.openjdk.org/browse/JDK-8287073
    
    - Expand Graal coverage to Java 17 and 21 on Linux, macOS, and Windows,
      using the current GraalVM setup without redundant Windows bootstrap.

--- project.clj
+++ project.clj
@@ -10,16 +10,20 @@
   :test-paths ["test" #_"src"]
 
   :dependencies
-  [[com.taoensso/encore              "3.158.0"]
-   [com.taoensso/nippy               "3.6.0"]
+  [[com.taoensso/encore              "3.169.1"]
+   [com.taoensso/nippy               "3.6.2"]
    [com.taoensso/trove               "1.1.0"]
+   ;; v2.13.x changes `GenericObjectPool.invalidateObject` (POOL-424)
+   ;; in a way that can mask errors (POOL-431); revisit after:
+   ;; https://github.com/apache/commons-pool/pull/454
    [org.apache.commons/commons-pool2 "2.12.1"]
-   [commons-codec/commons-codec      "1.20.0"]]
+   [commons-codec/commons-codec      "1.22.0"]]
 
   :profiles
   {;; :default [:base :system :user :provided :dev]
-   :provided {:dependencies [[org.clojure/clojure "1.12.3"]]}
-   :c1.12    {:dependencies [[org.clojure/clojure "1.12.3"]]}
+   :provided {:dependencies [[org.clojure/clojure "1.12.5"]]}
+   :c1.13    {:dependencies [[org.clojure/clojure "1.13.0-alpha4"]]}
+   :c1.12    {:dependencies [[org.clojure/clojure "1.12.5"]]}
    :c1.11    {:dependencies [[org.clojure/clojure "1.11.4"]]}
    :c1.10    {:dependencies [[org.clojure/clojure "1.10.3"]]}
 
@@ -29,8 +33,8 @@
     :aot [taoensso.graal-tests]
     :uberjar-name "graal-tests.jar"
     :dependencies
-    [[org.clojure/clojure                  "1.11.1"]
-     [com.github.clj-easy/graal-build-time "1.0.5"]]}
+    [[org.clojure/clojure                  "1.11.4"]
+     [com.github.clj-easy/graal-build-time "1.0.6"]]}
 
    :dev
    {;; :jvm-opts ["-server" "-Dtaoensso.elide-deprecated=true"]
@@ -39,16 +43,20 @@
      *assert*             true
      *unchecked-math*     false #_:warn-on-boxed}
 
+    ;; clj-aws-s3's legacy AWS SDK declares joda-time with an open version range.
+    :managed-dependencies
+    [[joda-time "2.14.2"]]
+
     :dependencies
-    [[org.clojure/test.check "1.1.1"]
-     [org.clojure/data.json  "2.5.1"]
+    [[org.clojure/test.check "1.1.3"]
+     [org.clojure/data.json  "2.5.2"]
      [com.taoensso/faraday   "1.12.3"]
      [clj-aws-s3             "0.3.10"]
-     [ring/ring-core         "1.15.3"]]
+     [ring/ring-core         "1.15.5"]]
 
     :plugins
     [[lein-pprint  "1.3.2"]
-     [lein-ancient "0.7.0"]]}}
+     [lein-ancient "1.0.0"]]}}
 
   :test-selectors
   {:v3 (fn [{:keys [ns]} & _] (.startsWith (str ns) "taoensso.carmine."))
@@ -59,8 +67,8 @@
    ;; "build-once" ["do" ["clean"] ["cljsbuild" "once"]]
    "deploy-lib"    ["do" #_["build-once"] ["deploy" "clojars"] ["install"]]
 
-   "test-v3"      ["with-profile" "+c1.12:+c1.11:+c1.10" "test" ":v3"]
-   "test-v4"      ["with-profile" "+c1.12:+c1.11:+c1.10" "test" ":v4"]
-   "test-clj"     ["with-profile" "+c1.12:+c1.11:+c1.10" "test"]
+   "test-v3"      ["with-profile" "+c1.13:+c1.12:+c1.11:+c1.10" "test" ":v3"]
+   "test-v4"      ["with-profile" "+c1.13:+c1.12:+c1.11:+c1.10" "test" ":v4"]
+   "test-clj"     ["with-profile" "+c1.13:+c1.12:+c1.11:+c1.10" "test"]
    ;; "test-cljs" ["with-profile" "+c1.12" "cljsbuild"         "test"]
    "test-all"     ["do" ["clean"] ["test-clj"] #_["test-cljs"]]})
